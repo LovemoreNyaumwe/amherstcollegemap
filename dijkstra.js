2dff3267272bc158595f30bf5916e9a313409c7c
LovemoreNyaumwe <lnyaumwe23@amherst.edu>
2023-04-25 22:00:33 -0400

    skeleton code.

---
+++ dijkstra.js
@@ -0,0 +1,22 @@
+// const image = document.getElementById('myImage');
+// const map = document.createElement('map');
+// map.setAttribute('name', 'myMap');
+
+// const area1 = document.createElement('area');
+// area1.setAttribute('shape', 'rect');
+// area1.setAttribute('coords', '0,0,100,100');
+// area1.setAttribute('href', 'https://www.example.com/page1.html');
+// area1.setAttribute('alt', 'Clickable area 1');
+
+// const area2 = document.createElement('area');
+// area2.setAttribute('shape', 'circle');
+// area2.setAttribute('coords', '150,150,50');
+// area2.setAttribute('href', 'https://www.example.com/page2.html');
+// area2.setAttribute('alt', 'Clickable area 2');
+
+// map.appendChild(area1);
+// map.appendChild(area2);
+
+// image.setAttribute('useMap', '#myMap');
+
+// document.body.appendChild(map);
